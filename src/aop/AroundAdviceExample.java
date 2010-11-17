1b93a9c049c69a67dfce8bb9a89f8c0af3a7b8dc
IanDarwin <>
2010-11-17 17:48:52 +0000

    Minor repairs.

--- src/aop/AroundAdviceExample.java
+++ src/aop/AroundAdviceExample.java
@@ -4,19 +4,19 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 
-@Aspect
 /**
  * Simple demonstration of "Around" advice.
- * Remember to add to Spring Bean Factory config too
+ * Remember to add aop config to Spring Bean Factory config.
  */
+@Aspect
 public class AroundAdviceExample {
 	
-	@Around("execution(* rain.RainForestService.*(..))")
+	@Around("execution(* main.SpringIntl.*(..))")
 	public Object around(ProceedingJoinPoint pjp) throws Throwable {
 		System.out.println("AroundAdviceExample.around()");
 		// do before work here
 		String methodName = pjp.getSignature().getName();
-		if (methodName.contains("Q")) {
+		if (methodName.contains("Q")) {	// slightly contrived example
 			throw new SecurityException("Invalid method " + methodName);
 		}
 		Object[] args = pjp.getArgs();
@@ -29,7 +29,7 @@ public class AroundAdviceExample {
 		try {
 			Object ret = pjp.proceed();
 			return ret;
-		finally {
+		} finally {
 			// do after work here, even if Exception thrown
 			System.out.println("Returning from " + methodName);
 		}
