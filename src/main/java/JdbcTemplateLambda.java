0b748e7c89b2a81bc9ad3fe772982204b05ef3da
Ian Darwin <ian@darwinsys.com>
2016-12-07 13:46:54 -0500

    Document, show optional uber-concise version of code.

--- src/main/java/JdbcTemplateLambda.java
+++ src/main/java/JdbcTemplateLambda.java
@@ -7,6 +7,13 @@ import model.MusicRecording;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
+/** Java 8 Lambda Expressions serve a variety of purposes;
+ * one of the more common uses is as a concise replacement
+ * for Inner Classes. This demo uses a Lambda Expression
+ * for a RowMapper<T> to avoid having to write an explicity
+ * inner class.
+ * @author Ian Darwin
+ */
 public class JdbcTemplateLambda {
 	DataSource dataSource = null;
 	
@@ -20,4 +27,15 @@ public class JdbcTemplateLambda {
 		rec.setTitle(rs.getString("title"));
 		return rec;
 	};
+	
+	// Note that if the mapper were only used in one place ever, we would
+	// write it inline, giving in an even shorter form:
+	public List<MusicRecording> getAllRecordingsConcise() {
+		return new JdbcTemplate(dataSource).query("Select * from MusicRecording", (rs, rownum) -> {
+			MusicRecording rec = new MusicRecording();
+			rec.setId(rs.getInt("id"));
+			rec.setTitle(rs.getString("title"));
+			return rec;
+		});
+	}
 }
